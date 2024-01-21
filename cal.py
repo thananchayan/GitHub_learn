e58a6828745af9e9087bc6e7c4c6af9976ed141d
Thavamohan Thananchayan <thananchayan26@gmail.com>
2024-01-22 00:31:03 +0530

    first commit

---
+++ cal.py
@@ -0,0 +1,15 @@
+# Get user inputs
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform calculations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Print the results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
